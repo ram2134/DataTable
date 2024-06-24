9b9ce0c30c6a3fda8868df357b6dc73ea9840672
Ramachandra <amnvramachandra213@gmail.com>
2024-06-24 08:24:24 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,16 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(
-  "mongodb+srv://amnvramachandra:ramachandra559@cluster0.gzkpmmq.mongodb.net/data_filtering",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
